3b17e502f7979e59a6adcfdc4624ba9b106cc10f
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-12-07 17:03:36 +0500

    added a link

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -2,6 +2,9 @@ package hexlet.code.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -9,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    String welcome() {
+        return "Welcome to Spring!";
     }
 }
