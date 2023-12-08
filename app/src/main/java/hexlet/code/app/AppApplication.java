f6d436a146d95344c28366ba4278e6e1ddb6db5a
SickJoke282 <menistr2@DESKTOP-VVJN9FC.localdomain>
2023-12-08 14:24:27 +0500

    added jpa auditing

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -2,10 +2,10 @@ package hexlet.code.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@RestController
+@EnableJpaAuditing
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
