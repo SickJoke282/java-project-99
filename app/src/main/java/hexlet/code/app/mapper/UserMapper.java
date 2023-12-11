d223d258864a78996ce635f4c9b90e9a9384bb6a
SickJoke282 <egorkrylatov18@gmail.com>
2023-12-11 17:26:56 +0500

    maps model to dto

--- app/src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -1,2 +1,32 @@
-package hexlet.code.app.mapper;public class UserMapper {
+package hexlet.code.app.mapper;
+
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.model.User;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.BeforeMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class UserMapper {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    public abstract User map(UserCreateDTO dto);
+    public abstract UserDTO map(User model);
+    public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+    @BeforeMapping
+    public void encryptPassword(UserCreateDTO data) {
+        var password = data.getPasswordDigest();
+        data.setPasswordDigest(passwordEncoder.encode(password));
+    }
 }
