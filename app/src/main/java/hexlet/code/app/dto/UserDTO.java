2ebb1bcb472904e637b7227017674c4e44e396a7
SickJoke282 <egorkrylatov18@gmail.com>
2023-12-11 17:29:39 +0500

    default dto for user model

--- app/src/main/java/hexlet/code/app/dto/UserDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserDTO.java
@@ -1,2 +1,20 @@
-package hexlet.code.app.dto;public class UserDTO {
+package hexlet.code.app.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UserDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Email
+    @NotBlank
+    private String email;
+    private LocalDate createdAt;
 }
