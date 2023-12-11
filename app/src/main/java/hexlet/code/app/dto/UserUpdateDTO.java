3b28bd4c8db51b8650141efabe4beb93e887ca3f
SickJoke282 <egorkrylatov18@gmail.com>
2023-12-11 17:28:47 +0500

    dto if user is being updated

--- app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,2 +1,18 @@
-package hexlet.code.app.dto;public class UserUpdateDTO {
+package hexlet.code.app.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserUpdateDTO {
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    @Size(min = 3)
+    private String passwordDigest;
 }
