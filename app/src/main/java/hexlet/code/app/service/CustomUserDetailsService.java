badd19146ad616bdb3d6c789b94023af89299323
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 18:15:19 -0500

    fixed creating user method

--- app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
@@ -2,16 +2,22 @@ package hexlet.code.app.service;
 
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsManager {
+    private final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
@@ -20,9 +26,16 @@ public class CustomUserDetailsService implements UserDetailsManager {
 
     @Override
     public void createUser(UserDetails userData) {
+        log.info(userData.getPassword());
+        if (userExists(userData.getUsername())) {
+            log.warn("User with username '{}' already exists.", userData.getUsername());
+            return;
+        }
         var user = new User();
         user.setEmail(userData.getUsername());
-        user.setPasswordDigest(userData.getPassword());
+        String encodedPassword = passwordEncoder.encode(userData.getPassword());
+        user.setPasswordDigest(encodedPassword);
+        log.info(user.getPassword());
         userRepository.save(user);
     }
 
@@ -46,7 +59,6 @@ public class CustomUserDetailsService implements UserDetailsManager {
 
     @Override
     public boolean userExists(String username) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'userExists'");
+        return userRepository.findByEmail(username).isPresent();
     }
 }
