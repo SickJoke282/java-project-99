d35c9d71797413fdc2e93b29d0c860efaadd6537
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 18:13:48 -0500

    controls auth

---
+++ app/src/main/java/hexlet/code/app/controller/AuthenticationController.java
@@ -0,0 +1,46 @@
+package hexlet.code.app.controller;
+
+import hexlet.code.app.dto.AuthRequest;
+import hexlet.code.app.model.User;
+import hexlet.code.app.utils.JWTUtils;
+import jakarta.validation.Valid;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticationController {
+    private final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public String login(@RequestBody @Valid AuthRequest request) {
+        try {
+            Authentication authenticate = authenticationManager
+                    .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                    request.getUsername(), request.getPassword()
+                            )
+                    );
+            User user = (User) authenticate.getPrincipal();
+            String encodedPassword = user.getPassword();
+            log.info("User '{}' successfully authenticated.", request.getUsername());
+            return jwtUtils.generateToken(request.getUsername());
+        } catch (BadCredentialsException ex) {
+            log.error("Authentication failed: Bad credentials", ex);
+            return "Incorrect";
+        }
+    }
+}
