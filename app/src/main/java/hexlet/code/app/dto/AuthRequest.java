a85325da2954bf8dfd1df88049dd58b91640fbe0
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 18:11:58 -0500

    dto for auth

---
+++ app/src/main/java/hexlet/code/app/dto/AuthRequest.java
@@ -0,0 +1,20 @@
+package hexlet.code.app.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+    @Column(unique = true)
+    @Email
+    @NotBlank
+    String username;
+    @NotBlank
+    @Size(min = 3)
+    String password;
+}
