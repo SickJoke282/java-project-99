91544ee8bfd12b88d17d127d6033b2fc296d888c
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 18:18:52 -0500

    deleted not blank from password

--- app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.app.dto;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,10 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserUpdateDTO {
+    @Column(unique = true)
     @Email
     @NotBlank
     private String email;
-    @NotBlank
     @Size(min = 3)
     private String passwordDigest;
 }
