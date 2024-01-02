e6290337c87c9627602326fc0e3b39bf4f7251d4
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 18:21:09 -0500

    added logs

--- app/src/main/java/hexlet/code/app/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.component;
 
 import hexlet.code.app.service.CustomUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -19,13 +21,18 @@ public class DataInitializer implements ApplicationRunner {
     private CustomUserDetailsService userService;
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
         userRepository.deleteAll();
         var email = "hexlet@example.com";
         var userData = new User();
         userData.setEmail(email);
         userData.setPasswordDigest("qwerty");
         userService.createUser(userData);
-
-        var user = userRepository.findByEmail(email).get();
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user != null) {
+            log.info("User has been found");
+        } else {
+            log.info("User has not been found");
+        }
     }
 }
