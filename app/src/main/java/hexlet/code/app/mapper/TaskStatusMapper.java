4dca284573fffbf6d89d88a0991dc981bbdb1376
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 20:59:05 -0500

    maps dtos

---
+++ app/src/main/java/hexlet/code/app/mapper/TaskStatusMapper.java
@@ -0,0 +1,28 @@
+package hexlet.code.app.mapper;
+
+import hexlet.code.app.dto.TaskStatusCreateDTO;
+import hexlet.code.app.dto.TaskStatusDTO;
+import hexlet.code.app.dto.TaskStatusUpdateDTO;
+import hexlet.code.app.model.TaskStatus;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskStatusMapper {
+    @Mapping(target = "author", source = "authorId")
+    public abstract TaskStatus map(TaskStatusCreateDTO dto);
+
+    @Mapping(source = "author.id", target = "authorId")
+    public abstract TaskStatusDTO map(TaskStatus model);
+
+    public abstract void update(TaskStatusUpdateDTO dto, @MappingTarget TaskStatus model);
+}
