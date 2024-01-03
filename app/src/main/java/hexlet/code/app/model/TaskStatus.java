6ec0e887e0804eab2915ba5fff1402b24cef9d26
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 21:04:16 -0500

    default model for task statuses

--- app/src/main/java/hexlet/code/app/model/TaskStatus.java
+++ app/src/main/java/hexlet/code/app/model/TaskStatus.java
@@ -1,2 +1,50 @@
-package hexlet.code.app.model;public class TaskStatus {
+package hexlet.code.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "task_statuses")
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class TaskStatus implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+    @JsonIgnore
+    @ManyToOne(optional = false)
+    @NotNull
+    private User author;
+    @Column(unique = true)
+    @NotBlank
+    @Size(min = 1)
+    private String name;
+    @Column(unique = true)
+    @NotBlank
+    @Size(min = 1)
+    private String slug;
+    @CreatedDate
+    private LocalDate createdAt;
 }
