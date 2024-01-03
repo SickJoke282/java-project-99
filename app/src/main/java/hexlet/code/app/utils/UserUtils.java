df470cf4f3e7ed725584d0252cdd64b1506dce78
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-02 21:03:24 -0500

    added is current user method

--- app/src/main/java/hexlet/code/app/utils/UserUtils.java
+++ app/src/main/java/hexlet/code/app/utils/UserUtils.java
@@ -1,6 +1,7 @@
 package hexlet.code.app.utils;
 
 import hexlet.code.app.model.User;
+import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 public class UserUtils {
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
     public User getCurrentUser() {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
@@ -22,5 +24,15 @@ public class UserUtils {
     public User getTestUser() {
         return userRepository.findByEmail("hexlet@example.com")
                 .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    public boolean isAuthor(long id) {
+        var testStatusAuthorEmail = taskStatusRepository.findById(id).get().getAuthor().getEmail();
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        return testStatusAuthorEmail.equals(authentication.getName());
+    }
+    public boolean isCurrentUser(long id) {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var currentUser = getCurrentUser();
+        return currentUser != null && currentUser.getEmail().equals(userRepository.findById(id).get().getUsername());
     }
 }
