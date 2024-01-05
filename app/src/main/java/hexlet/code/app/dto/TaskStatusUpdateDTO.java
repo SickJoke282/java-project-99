96883f0f24af5d1edcb9d45adf4eaf8cac01e6c8
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-05 18:46:32 -0500

    fix

--- app/src/main/java/hexlet/code/app/dto/TaskStatusUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/TaskStatusUpdateDTO.java
@@ -1,10 +1,13 @@
 package hexlet.code.app.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
+@Getter
+@Setter
 public class TaskStatusUpdateDTO {
-    @NotBlank
     @Size(min = 1)
-    private String name;
+    private JsonNullable<String> name;
 }
