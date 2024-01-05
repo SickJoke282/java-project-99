74c94a4df7da5e4d5f24c9da4d2194b133d326a8
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-05 18:48:12 -0500

    repo for tasks

---
+++ app/src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package hexlet.code.app.repository;
+
+import hexlet.code.app.model.Task;
+import hexlet.code.app.model.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByName(String name);
+    Optional<Task> findByTaskStatus(TaskStatus taskStatus);
+}
