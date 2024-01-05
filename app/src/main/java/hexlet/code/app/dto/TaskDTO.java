9affb691ac65127436d29b48c75d54df68bae2a1
SickJoke282 <egorkrylatov18@gmail.com>
2024-01-05 18:46:48 -0500

    dtos for tasks

--- app/src/main/java/hexlet/code/app/dto/TaskDTO.java
+++ app/src/main/java/hexlet/code/app/dto/TaskDTO.java
@@ -2,21 +2,20 @@ package hexlet.code.app.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import hexlet.code.app.model.TaskStatus;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
-public class TaskCreateDTO {
-    @NotBlank
+public class TaskDTO {
+    private Long id;
     private String title;
     private Integer index;
     private String content;
     private String status;
     @JsonProperty("assignee_id")
-    private Long assigneeId;}
+    private Integer assigneeId;
+    private LocalDate createdAt;
+}
---
+++ app/src/main/java/hexlet/code/app/dto/TaskUpdateDTO.java
@@ -0,0 +1,20 @@
+package hexlet.code.app.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Getter
+@Setter
+public class TaskUpdateDTO {
+    @NotNull
+    private JsonNullable<String> title;
+    private JsonNullable<Integer> index;
+    private JsonNullable<String> content;
+    private JsonNullable<String> status;
+    @JsonProperty("assignee_id")
+    private JsonNullable<Long> assigneeId;
+}
